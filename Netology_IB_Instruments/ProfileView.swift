b1cc4886c8639b647ce7ee6ed0bf47470bcd9938
Konstantin Bolgar-Danchenko <constantine.bolgar@icloud.com>
2022-06-21 08:34:05 +0700

    xib created

---
+++ Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Konstantin Bolgar-Danchenko on 21.06.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var UserPhoto: UIImageView!
+    @IBOutlet weak var UserName: UILabel!
+    @IBOutlet weak var UserBirthday: UILabel!
+    @IBOutlet weak var UserLocation: UILabel!
+    @IBOutlet weak var UserInfo: UITextView!
+
+}
